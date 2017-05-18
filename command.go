0b11e98db471c7b33cbc210f818c37c548440575
sluceno <sluceno.a@gmail.com>
2017-05-18 12:21:14 +0200

    Provide initialization methods for commands to dry things up.

--- command.go
+++ command.go
@@ -1,6 +1,7 @@
 package chuper
 
 import (
+	"errors"
 	"net/http"
 	"net/url"
 
@@ -24,6 +25,27 @@ type Cmd struct {
 	R int
 }
 
+func NewCmd(method, urlStr, sourceURLStr string, depth, retries int) (Cmd, error) {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return Cmd{}, err
+	}
+
+	s, err := url.Parse(sourceURLStr)
+	if err != nil {
+		return Cmd{}, err
+	}
+
+	c := Cmd{
+		Cmd: &fetchbot.Cmd{U: u, M: method},
+		S:   s,
+		D:   depth,
+		R:   retries,
+	}
+
+	return c, nil
+}
+
 func (c *Cmd) SourceURL() *url.URL {
 	return c.S
 }
@@ -45,6 +67,27 @@ type CmdBasicAuth struct {
 	user, pass string
 }
 
+func NewCmdBasiAuth(method, urlStr, sourceURLStr string, depth, retries int, user, password string) (CmdBasicAuth, error) {
+	var cmd CmdBasicAuth
+
+	if user == "" && password == "" {
+		return cmd, errors.New("user or password not provided")
+	}
+
+	baseCommand, err := NewCmd(method, urlStr, sourceURLStr, depth, retries)
+	if err != nil {
+		return cmd, err
+	}
+
+	cmd = CmdBasicAuth{
+		Cmd:  baseCommand,
+		user: user,
+		pass: password,
+	}
+
+	return cmd, nil
+}
+
 func (c *CmdBasicAuth) BasicAuth() (string, string) {
 	return c.user, c.pass
 }
@@ -52,6 +95,25 @@ func (c *CmdBasicAuth) BasicAuth() (string, string) {
 type CmdHeader struct {
 	Cmd
 	header http.Header
+}
+
+func NewCmdHeader(method, urlStr, sourceURLStr string, depth, retries int, header http.Header) (CmdHeader, error) {
+	var cmd CmdHeader
+	if header == nil {
+		return cmd, errors.New("header not provided")
+	}
+
+	baseCommand, err := NewCmd(method, urlStr, sourceURLStr, depth, retries)
+	if err != nil {
+		return cmd, err
+	}
+
+	cmd = CmdHeader{
+		Cmd:    baseCommand,
+		header: header,
+	}
+
+	return cmd, nil
 }
 
 func (c *CmdHeader) Header() http.Header {
